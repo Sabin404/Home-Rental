9c6130e227d3958af17cab34792b8d4addff2e94
Sabin <paudelsabin0@gmail.com>
2024-06-11 21:54:49 +0545

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import './externalFiles/search.js'
+
 const home_images = [
   "https://th.bing.com/th/id/OIP.efnJ95bSkFJqx7LYn9jCWQHaE3?rs=1&pid=ImgDetMain",
   "https://th.bing.com/th/id/OIP._ornIK8XKAE6nVoXpK9TlQHaEY?w=760&h=450&rs=1&pid=ImgDetMain",
@@ -17,16 +19,16 @@ const home_images = [
 ];
 
 const home_details = [
-  { price: "$200,000", text: "Beautiful Family House" },
-  { price: "$300,000", text: "Modern Apartment" },
-  { price: "$150,000", text: "Cozy Cottage" },
-  { price: "$250,000", text: "Spacious Villa" },
-  { price: "$180,000", text: "Stylish Condo" },
-  { price: "$400,000", text: "Luxurious Mansion" },
-  { price: "$350,000", text: "Urban Loft" },
-  { price: "$220,000", text: "Suburban Home" },
-  { price: "$275,000", text: "Elegant Duplex" },
-  { price: "$320,000", text: "Rural Farmhouse" }
+  { price: "$200,000", text: "Beautiful Family House" ,city:"Pokhara" },
+  { price: "$300,000", text: "Modern Apartment",city:"Pokhara" },
+  { price: "$150,000", text: "Cozy Cottage" ,city:"Pokhara"},
+  { price: "$250,000", text: "Spacious Villa" ,city:"Pokhara"},
+  { price: "$180,000", text: "Stylish Condo" ,city:"Pokhara"},
+  { price: "$400,000", text: "Luxurious Mansion" ,city:"Pokhara"},
+  { price: "$350,000", text: "Urban Loft" ,city:"Pokhara"},
+  { price: "$220,000", text: "Suburban Home" ,city:"Pokhara"},
+  { price: "$275,000", text: "Elegant Duplex" ,city:"Pokhara"},
+  { price: "$320,000", text: "Rural Farmhouse" ,city:"Pokhara"}
 ];
 const image = document.querySelector('.image');
 // image.src = home_images[0];
@@ -75,7 +77,37 @@ r_houses.forEach((src, index) => {
     rhouse.appendChild(houseItem);
 });
 
-//TOOGLE BUTTON
+const head=document.querySelector('head');
+const li=document.createElement('link');
+li.rel="icon";
+li.href=home_images[3];
+head.appendChild(li);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const searchButton=document.querySelector('#searchbutton');
+const searchCity=document.querySelector('#searchCity');
+const minprice=document.querySelector('#minprice');
+const maxprice=document.querySelector('#maxprice');
+const searchForm = document.querySelector('#searchForm');
 
 
+function submit(e){
+  e.preventDefault();
+  let city=searchCity.value.toLowerCase();
+  let minP=parseInt(minprice.value);
+  let maxP=parseInt(maxprice.value);
+
+  if (city && !isNaN(minP) && !isNaN(maxP)) {
+    localStorage.setItem('city', city);
+    localStorage.setItem('min_Price', minP);
+    localStorage.setItem('max_Price', maxP);
+    window.location.href = './externalFiles/search.html';
+} else {
+    alert('Please enter valid values for all fields.');
+}
+}
+
+searchForm.addEventListener('submit',submit);
+});
 
